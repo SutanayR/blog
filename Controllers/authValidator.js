413a839515596ea5b2542963ab9e615e10c3f943
bhat.akash <bhatakash>
2022-09-12 13:01:46 +0530

    Fix coding standards

--- Controllers/authValidator.js
+++ Controllers/authValidator.js
@@ -1,31 +1,24 @@
-const {validatorSchema} = require("../Model/validateSchema")
-const handleError=(err)=>
-{
-    const errors={email:'',password:'',number:''};
-    if(err.message.includes('E11000 duplicate key'))
-    errors.email="Email already exists";
-    if(err.message.includes('"email" must be a valid email'))
-    errors.email="Enter valid email";
-    if(err.message.includes('password'))
-    errors.password=err.message;
-    if(err.message.includes('"number"'))
-    errors.number=err.message;
+const { validatorSchema } = require('../Model/validateSchema');
+
+const handleError = (err) => {
+  const errors = { email: '', password: '', number: '' };
+  if (err.message.includes('E11000 duplicate key')) { errors.email = 'Email already exists'; }
+  if (err.message.includes('"email" must be a valid email')) { errors.email = 'Enter valid email'; }
+  if (err.message.includes('password')) { errors.password = err.message; }
+  if (err.message.includes('"number"')) { errors.number = err.message; }
+  return errors;
+};
+const validator = async (email, password, number) => {
+  try {
+    await validatorSchema.validateAsync({
+      email,
+      password,
+      number,
+    });
+    return true;
+  } catch (err) {
+    const errors = handleError(err);
     return errors;
-}
-const validator=async (email,password,number)=>{
-    try
-    {
-        const result=await validatorSchema.validateAsync({
-            email:email,
-            password:password,
-            number:number
-        });
-        return true;
-    }
-    catch(err)
-    {
-        const errors=handleError(err);
-        return errors;
-    }
-}
-module.exports ={validator};
+  }
+};
+module.exports = { validator };
--- Model/validateSchema.js
+++ Model/validateSchema.js
@@ -1,7 +1,8 @@
-const Joi=require('joi');
-const validatorSchema=Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(6).required(),
-    number: Joi.string().min(10).pattern(/^\d+$/).required()
+const Joi = require('joi');
+
+const validatorSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+  number: Joi.string().min(10).pattern(/^\d+$/).required(),
 });
-module.exports ={validatorSchema};
+module.exports = { validatorSchema };
